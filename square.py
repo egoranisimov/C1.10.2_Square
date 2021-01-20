6479c563e8a82550a6abdac6feeb67be3bb6d6d4
eanisimov <egoranisimov@gmail.com>
2021-01-20 21:31:43 +0300

    v1.0

---
+++ square.py
@@ -0,0 +1,11 @@
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def get_area(self):
+        return self.width * self.height
+
+
+rect_1 = Rectangle(4, 7)
+print(rect_1.get_area())
